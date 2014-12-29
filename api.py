3b4140baccf4b4e709fcccf80180bb14d624e219
theepicsnail <theepicsnail@gmail.com>
2014-12-28 20:24:22 -0800

    Replaces api.py with the generated version

--- api.py
+++ api.py
@@ -13,30 +13,28 @@ class NonterminalRule(Node):
   IMPL = impl.NonterminalRule
   @staticmethod
   def matcher():
-    return (NAME, ":", NEWLINE, INDENT,
-        MANY(Rule), DEDENT)
+    return (NAME, ":", NEWLINE, INDENT, MANY(Rule), DEDENT)
 
 class Rule(Node):
   IMPL = impl.Rule
   @staticmethod
   def matcher():
-    return MANY(Matcher), NEWLINE
+    return (MANY(Matcher), NEWLINE)
 
 class Matcher(Node):
   IMPL = impl.Matcher
   @staticmethod
   def matcher():
-    return Items, ZERO_OR_MORE(("|", Items))
+    return (Items, ZERO_OR_MORE(("|", Items)))
 
 class Items(Node):
   IMPL = impl.Items
   @staticmethod
   def matcher():
-    return MANY(ANY(NAME, Group, STRING))
+    return MANY(ANY(NAME, STRING, Group))
 
 class Group(Node):
   IMPL = impl.Group
   @staticmethod
   def matcher():
-    return ("(", Matcher, ")", OPTIONAL(ANY("+", "?", "*")))
-
+    return ("(", Matcher, ")", OPTIONAL(ANY("?", "+", "*")))
